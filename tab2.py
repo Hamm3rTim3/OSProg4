3cbb4bc0afd859c90b9627a4231d29f61e838f12
HanSolo787 <adam.lawson@mines.sdsmt.edu>
2016-05-01 15:14:43 -0600

    "Generated random reference string and fixed when button clicks"

--- tab2.py
+++ tab2.py
@@ -31,3 +31,33 @@ class Tab2(Frame):
         self.numTLBValue = StringVar()
         self.numTLBEntry = Entry(self, textvariable = self.numTLBValue)
         self.numTLBEntry.grid(row=1, column=6)
+
+        self.generateButton = Button(self, text = "Simulate", command = self.inputCheck)
+        self.generateButton.grid(row=2, column=1)
+
+    def inputCheck(self):
+        self.warningLabel = "Danger, Danger!!!"
+
+        if self.numFramesValue.get().isnumeric() == False or self.numFramesValue.get() is None :
+            messagebox.showerror(self.warningLabel, "You must enter a number.")
+            return
+
+        elif int(self.numFramesValue.get()) > 10:
+            messagebox.showwarning(self.warningLabel, "Please use no more than 10 frames.")
+            return
+
+        elif self.numPagesValue.get().isnumeric() == False:
+            messagebox.showerror(self.warningLabel, "You must enter a number.")
+            return
+
+        elif int(self.numPagesValue.get()) > 20:
+                messagebox.showwarning(self.warningLabel, "Please use no more than 20 pages.")
+                return
+
+        elif self.numTLBValue.get().isnumeric() == False:
+            messagebox.showerror(self.warningLabel, "You must enter a number.")
+            return
+
+        elif int(self.numTLBValue.get()) > 10:
+                messagebox.showwarning(self.warningLabel, "Please use a size no larger than 20.")
+                return
